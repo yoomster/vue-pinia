9b8749bc99c31db7bf0b0af92c1a7d4243847810
yoomster <nperenboom@hotmail.com>
2025-05-18 20:25:13 +0200

    updates

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,10 +1,19 @@
 <template>
   <h1>Vue - pinia counter</h1>
+  <h2>{{ counter }}</h2>
+  <div>
+    <button @click="increment()" class="btn btn-success">+1</button>
+    <button @click="decrement()" class="btn btn-info">-1</button>
+    <button @click="reset()" class="btn btn-danger">Reset</button>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Counter",
+  data() {
+    counter = 0;
+  },
 };
 </script>
 
