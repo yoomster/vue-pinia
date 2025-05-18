34d10b2dcf71638aaf81435f65be60f001b0d708
yoomster <nperenboom@hotmail.com>
2025-05-18 21:15:05 +0200

    shared state 4 diff components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Counter from "@/components/Counter.vue";
+import AnotherCounter from "@/components/AnotherCounter.vue";
 </script>
 
 <template>
   <Counter />
+  <AnotherCounter />
 </template>
 
 <style scoped></style>
---
+++ src/components/AnotherCounter.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2>2nd counter = also {{ counter }}</h2>
+</template>
+
+<script>
+import { useCounterStore } from "@/stores/counterStore";
+export default {
+  name: "AnotherCounter",
+  data() {
+    return {
+      store: useCounterStore(),
+    };
+  },
+  computed: {
+    counter() {
+      return this.store.counter;
+    },
+  },
+};
+</script>
